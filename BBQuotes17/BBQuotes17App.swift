84529e76009f593e3462831e0678aaab9897d915
cloviscarmezini <cloviscarmezini14@gmail.com>
2024-06-06 19:16:50 -0300

    Initial Commit

---
+++ BBQuotes17/BBQuotes17App.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotes17App.swift
+//  BBQuotes17
+//
+//  Created by Clovis Carmezini on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotes17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
