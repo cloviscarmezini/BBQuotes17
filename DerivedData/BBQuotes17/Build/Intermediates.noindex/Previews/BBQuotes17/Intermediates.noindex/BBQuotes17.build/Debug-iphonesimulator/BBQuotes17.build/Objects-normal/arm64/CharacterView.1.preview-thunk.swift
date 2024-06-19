31f29b92f4a10a05e2ba2bec1b8d0d6799124298
cloviscarmezini <cloviscarmezini14@gmail.com>
2024-06-18 22:32:45 -0300

    added animation and action to scroll to botton on press status

--- DerivedData/BBQuotes17/Build/Intermediates.noindex/Previews/BBQuotes17/Intermediates.noindex/BBQuotes17.build/Debug-iphonesimulator/BBQuotes17.build/Objects-normal/arm64/CharacterView.1.preview-thunk.swift
+++ DerivedData/BBQuotes17/Build/Intermediates.noindex/Previews/BBQuotes17/Intermediates.noindex/BBQuotes17.build/Debug-iphonesimulator/BBQuotes17.build/Objects-normal/arm64/CharacterView.1.preview-thunk.swift
@@ -12,91 +12,104 @@ extension CharacterView {
     @_dynamicReplacement(for: body) private var __preview__body: some View {
         #sourceLocation(file: "/Users/cloviscarmezini/swift/BBQuotes17/BBQuotes17/CharacterView.swift", line: 15)
         GeometryReader { geo in
-            ZStack(alignment: .top) {
-                Image(show.lowercased().replacingOccurrences(of: __designTimeString("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[0].arg[0].value.modifier[1].arg[0].value", fallback: " "), with: __designTimeString("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[0].arg[0].value.modifier[1].arg[1].value", fallback: "")))
-                    .resizable()
-                    .scaledToFit()
-                
-                ScrollView {
-                    AsyncImage(url: character.images[__designTimeInteger("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[0].arg[0].value.[0].value", fallback: 0)]) { image in
-                        image
-                            .resizable()
-                            .scaledToFill()
-                        
-                    } placeholder: {
-                        ProgressView()
-                    }
-                    .frame(width: geo.size.width/__designTimeFloat("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[0].modifier[0].arg[0].value.[0]", fallback: 1.2), height: geo.size.height / __designTimeFloat("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[0].modifier[0].arg[1].value.[0]", fallback: 1.7))
-                    .clipShape(.rect(cornerRadius: __designTimeInteger("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[0].modifier[1].arg[0].value.arg[0].value", fallback: 25)))
-                    .padding(.top, __designTimeInteger("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[0].modifier[2].arg[1].value", fallback: 60))
+            ScrollViewReader { proxy in
+                ZStack(alignment: .top) {
+                    Image(show.lowercased().replacingOccurrences(of: __designTimeString("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[0].arg[0].value.modifier[1].arg[0].value", fallback: " "), with: __designTimeString("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[0].arg[0].value.modifier[1].arg[1].value", fallback: "")))
+                        .resizable()
+                        .scaledToFit()
                     
-                    VStack(alignment: .leading) {
-                        Text(character.name)
-                            .font(.largeTitle)
-                        
-                        Text("Portrayed By: \(character.portrayedBy)")
-                            .font(.subheadline)
-                        
-                        Divider()
-                        
-                        Text("\(character.name) Character Info")
-                            .font(.title2)
-                        
-                        Text("Born: \(character.birthday)")
-                        
-                        Divider()
-                        
-                        Text(__designTimeString("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].arg[1].value.[6].arg[0].value", fallback: "Occupations:"))
-                        
-                        ForEach(character.occupations, id: \.self) { occupation in
-                            Text("• \(occupation)")
-                                .font(.subheadline)
+                    ScrollView {
+                        TabView {
+                            ForEach(character.images, id: \.self) { imageURL in
+                                AsyncImage(url: imageURL) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                    
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                            }
                         }
-                        
-                        Divider()
-                        
-                        Text(__designTimeString("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].arg[1].value.[9].arg[0].value", fallback: "Nicknames:"))
-                        
-                        if character.aliases.count > 0 {
-                            ForEach(character.aliases, id: \.self) { alias in
-                                Text("• \(alias)")
+                        .tabViewStyle(.page)
+                            .frame(width: geo.size.width/__designTimeFloat("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[0].modifier[1].arg[0].value.[0]", fallback: 1.2), height: geo.size.height / __designTimeFloat("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[0].modifier[1].arg[1].value.[0]", fallback: 1.7))
+                            .clipShape(.rect(cornerRadius: __designTimeInteger("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[0].modifier[2].arg[0].value.arg[0].value", fallback: 25)))
+                            .padding(.top, __designTimeInteger("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[0].modifier[3].arg[1].value", fallback: 60))
+
+                        VStack(alignment: .leading) {
+                            Text(character.name)
+                                .font(.largeTitle)
+                            
+                            Text("Portrayed By: \(character.portrayedBy)")
+                                .font(.subheadline)
+                            
+                            Divider()
+                            
+                            Text("\(character.name) Character Info")
+                                .font(.title2)
+                            
+                            Text("Born: \(character.birthday)")
+                            
+                            Divider()
+                            
+                            Text(__designTimeString("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].arg[1].value.[6].arg[0].value", fallback: "Occupations:"))
+                            
+                            ForEach(character.occupations, id: \.self) { occupation in
+                                Text("• \(occupation)")
                                     .font(.subheadline)
                             }
-                        } else {
-                            Text(__designTimeString("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].arg[1].value.[10].[1].[0].arg[0].value", fallback: "None"))
-                                .font(.subheadline)
-                        }
-                        
-                        Divider()
-                        
-                        DisclosureGroup(__designTimeString("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].arg[1].value.[12].arg[0].value", fallback: "Status: (spoiler alert!)")) {
-                            VStack(alignment: .leading) {
-                                Text(character.status)
-                                    .font(.title2)
-                                
-                                if let death = character.death {
-                                    AsyncImage(url: death.image) { image in
-                                        image
-                                            .resizable()
-                                            .scaledToFit()
-                                            .clipShape(.rect(cornerRadius: __designTimeInteger("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].arg[1].value.[12].arg[1].value.[0].arg[1].value.[1].[0].[0].arg[1].value.[0].modifier[2].arg[0].value.arg[0].value", fallback: 15)))
-                                        
-                                    } placeholder: {
-                                        ProgressView()
-                                    }
-                                    
-                                    Text("How: \(death.details)")
-                                        .padding(.bottom, __designTimeInteger("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].arg[1].value.[12].arg[1].value.[0].arg[1].value.[1].[0].[1].modifier[0].arg[1].value", fallback: 7))
-                                    
-                                    Text("Last words: \(death.lastWords)")
+                            
+                            Divider()
+                            
+                            Text(__designTimeString("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].arg[1].value.[9].arg[0].value", fallback: "Nicknames:"))
+                            
+                            if character.aliases.count > 0 {
+                                ForEach(character.aliases, id: \.self) { alias in
+                                    Text("• \(alias)")
+                                        .font(.subheadline)
                                 }
-                            }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-                        }.tint(.primary)
+                            } else {
+                                Text(__designTimeString("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].arg[1].value.[10].[1].[0].arg[0].value", fallback: "None"))
+                                    .font(.subheadline)
+                            }
+                            
+                            Divider()
+                            
+                            DisclosureGroup(__designTimeString("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].arg[1].value.[12].arg[0].value", fallback: "Status: (spoiler alert!)")) {
+                                VStack(alignment: .leading) {
+                                    Text(character.status)
+                                        .font(.title2)
+                                    
+                                    if let death = character.death {
+                                        AsyncImage(url: death.image) { image in
+                                            image
+                                                .resizable()
+                                                .scaledToFit()
+                                                .clipShape(.rect(cornerRadius: __designTimeInteger("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].arg[1].value.[12].arg[1].value.[0].arg[1].value.[1].[0].[0].arg[1].value.[0].modifier[2].arg[0].value.arg[0].value", fallback: 15)))
+                                                .onAppear {
+                                                    withAnimation {
+                                                        proxy.scrollTo(__designTimeInteger("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].arg[1].value.[12].arg[1].value.[0].arg[1].value.[1].[0].[0].arg[1].value.[0].modifier[3].arg[0].value.[0].arg[0].value.[0].modifier[0].arg[0].value", fallback: 1), anchor: .bottom)
+                                                    }
+                                                }
+                                            
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
+                                        
+                                        Text("How: \(death.details)")
+                                            .padding(.bottom, __designTimeInteger("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].arg[1].value.[12].arg[1].value.[0].arg[1].value.[1].[0].[1].modifier[0].arg[1].value", fallback: 7))
+                                        
+                                        Text("Last words: \(death.lastWords)")
+                                    }
+                                }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                            }.tint(.primary)
+                        }
+                        .frame(width: geo.size.width / __designTimeFloat("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].modifier[0].arg[0].value.[0]", fallback: 1.25), alignment: .leading)
+                        .padding(.bottom, __designTimeInteger("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].modifier[1].arg[1].value", fallback: 50))
+                        .id(__designTimeInteger("#5189.[1].[2].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].modifier[2].arg[0].value", fallback: 1))
                     }
-                    .frame(width: geo.size.width / __designTimeFloat("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].modifier[0].arg[0].value.[0]", fallback: 1.25), alignment: .leading)
-                    .padding(.bottom, __designTimeInteger("#6879.[1].[2].property.[0].[0].arg[0].value.[0].arg[1].value.[1].arg[0].value.[1].modifier[1].arg[1].value", fallback: 50))
+                    .scrollIndicators(.hidden)
                 }
-                .scrollIndicators(.hidden)
             }
         }
         .ignoresSafeArea()
--- DerivedData/BBQuotes17/Build/Intermediates.noindex/Previews/BBQuotes17/Intermediates.noindex/BBQuotes17.build/Debug-iphonesimulator/BBQuotes17.build/Objects-normal/arm64/ContentView.1.preview-thunk.swift
+++ DerivedData/BBQuotes17/Build/Intermediates.noindex/Previews/BBQuotes17/Intermediates.noindex/BBQuotes17.build/Debug-iphonesimulator/BBQuotes17.build/Objects-normal/arm64/ContentView.1.preview-thunk.swift
@@ -12,18 +12,18 @@ extension ContentView {
     @_dynamicReplacement(for: body) private var __preview__body: some View {
         #sourceLocation(file: "/Users/cloviscarmezini/swift/BBQuotes17/BBQuotes17/ContentView.swift", line: 12)
         TabView {
-            QuoteView(show: __designTimeString("#981.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value", fallback: "Breaking Bad"))
+            QuoteView(show: __designTimeString("#5140.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value", fallback: "Breaking Bad"))
                 .tabItem {
-                    Label(__designTimeString("#981.[1].[0].property.[0].[0].arg[0].value.[0].modifier[0].arg[0].value.[0].arg[0].value", fallback: "Breaking Bad"), systemImage: __designTimeString("#981.[1].[0].property.[0].[0].arg[0].value.[0].modifier[0].arg[0].value.[0].arg[1].value", fallback: "tortoise"))
+                    Label(__designTimeString("#5140.[1].[0].property.[0].[0].arg[0].value.[0].modifier[0].arg[0].value.[0].arg[0].value", fallback: "Breaking Bad"), systemImage: __designTimeString("#5140.[1].[0].property.[0].[0].arg[0].value.[0].modifier[0].arg[0].value.[0].arg[1].value", fallback: "tortoise"))
                 }.toolbarBackground(.visible, for: .tabBar)
             
-            QuoteView(show: __designTimeString("#981.[1].[0].property.[0].[0].arg[0].value.[1].arg[0].value", fallback: "Better Call Saul"))
+            QuoteView(show: __designTimeString("#5140.[1].[0].property.[0].[0].arg[0].value.[1].arg[0].value", fallback: "Better Call Saul"))
                 .tabItem {
-                    Label(__designTimeString("#981.[1].[0].property.[0].[0].arg[0].value.[1].modifier[0].arg[0].value.[0].arg[0].value", fallback: "Better Call Saul"), systemImage: __designTimeString("#981.[1].[0].property.[0].[0].arg[0].value.[1].modifier[0].arg[0].value.[0].arg[1].value", fallback: "briefcase"))
+                    Label(__designTimeString("#5140.[1].[0].property.[0].[0].arg[0].value.[1].modifier[0].arg[0].value.[0].arg[0].value", fallback: "Better Call Saul"), systemImage: __designTimeString("#5140.[1].[0].property.[0].[0].arg[0].value.[1].modifier[0].arg[0].value.[0].arg[1].value", fallback: "briefcase"))
                 }.toolbarBackground(.visible, for: .tabBar)
-            QuoteView(show: __designTimeString("#981.[1].[0].property.[0].[0].arg[0].value.[2].arg[0].value", fallback: "Better Call Saul"))
+            QuoteView(show: __designTimeString("#5140.[1].[0].property.[0].[0].arg[0].value.[2].arg[0].value", fallback: "El Camino"))
                 .tabItem {
-                    Label(__designTimeString("#981.[1].[0].property.[0].[0].arg[0].value.[2].modifier[0].arg[0].value.[0].arg[0].value", fallback: "Better Call Saul"), systemImage: __designTimeString("#981.[1].[0].property.[0].[0].arg[0].value.[2].modifier[0].arg[0].value.[0].arg[1].value", fallback: "briefcase"))
+                    Label(__designTimeString("#5140.[1].[0].property.[0].[0].arg[0].value.[2].modifier[0].arg[0].value.[0].arg[0].value", fallback: "El Camino"), systemImage: __designTimeString("#5140.[1].[0].property.[0].[0].arg[0].value.[2].modifier[0].arg[0].value.[0].arg[1].value", fallback: "car"))
                 }.toolbarBackground(.visible, for: .tabBar)
         }.preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
     
